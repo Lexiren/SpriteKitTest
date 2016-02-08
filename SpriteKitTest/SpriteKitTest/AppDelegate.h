a9c1779e49a0ad9e87e70d6219bcdcd06a032d55
Oleksii.Maidanyk <omaid@softserveinc.com>
2016-02-08 16:38:10 +0200

    Initial commit

---
+++ SpriteKitTest/SpriteKitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteKitTest
+//
+//  Created by Oleksii Maidanyk on 2/8/16.
+//  Copyright © 2016 EasyDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
